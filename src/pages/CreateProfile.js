dd2d65928c1272b38126de3b86fcba9365981ab6
Ksishore <satyakishore04@gmail.com>
2023-05-23 14:37:41 +0530

    POST api in create Profile

--- src/pages/CreateProfile.js
+++ src/pages/CreateProfile.js
@@ -1,15 +1,145 @@
+import React, { useState } from "react";
 import "antd/dist/antd.min.css";
 import { Input, Switch, Button, Col, Row, Card, Checkbox, Form } from "antd";
 import { CloseOutlined } from "@ant-design/icons";
+import axios from "axios";
 import styles from "./CreateProfile.module.css";
 const { TextArea } = Input;
+
 const CreateProfile = () => {
-  const onFinish = (values) => {
-    console.log("Success:", values);
+  const [form] = Form.useForm();
+
+  const [isVerified, setIsVerified] = useState(false);
+  const [error, setError] = useState(null);
+
+  // const handleSubmit = async (values) => {
+  //   try {
+  //     const response = await axios.post(
+  //       "https://api.poc.graphql.dev.vnplatform.com/graphql",
+  //       {
+  //         query: `
+  //           mutation CreateProfile(
+  //             $firstName: String!,
+  //             $lastName: String!,
+  //             $email: String!,
+  //             $isVerified: Boolean!,
+  //             $imageUrl: String!,
+  //             $description: String!
+  //           ) {
+  //             createProfile(
+  //               first_name: $firstName,
+  //               last_name: $lastName,
+  //               email: $email,
+  //               is_verified: $isVerified,
+  //               image_url: $imageUrl,
+  //               description: $description
+  //             ) {
+  //               id
+  //               first_name
+  //               last_name
+  //               email
+  //               is_verified
+  //               image_url
+  //               description
+  //             }
+  //           }
+  //         `,
+  //         variables: {
+  //           firstName,
+  //           lastName,
+  //           email,
+  //           isVerified,
+  //           imageUrl,
+  //           description,
+  //         },
+  //       },
+  //       {
+  //         headers: {
+  //           Authorization:
+  //             "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjYW5kaWRhdGVfbmFtZSI6IlNpbWFyIiwiaXNfY2FuZGlkYXRlIjp0cnVlLCJpYXQiOjE2NzcyNzAxNTMsImV4cCI6MTY3NzYxNTc1M30.OERVKTr3kCq5cS_QzRWmiEfbPHemMPlW6GXep7IyvA0",
+  //         },
+  //       }
+  //     );
+  //     console.log(response);
+  //     if (response.data) {
+  //       document.getElementById("createProfile").reset();
+  //       window.location.href = "/";
+  //     }
+  //   } catch (error) {
+  //     setError(error.message);
+  //   }
+  // };
+
+  const handleSubmit = async (values) => {
+    const {
+      first_name: firstName,
+      last_name: lastName,
+      email,
+      image_url: imageUrl,
+      description,
+    } = values;
+
+    values["is_verified"] = isVerified;
+
+    console.log(values, "values");
+    // return;
+    try {
+      const response = await axios.post(
+        "https://api.poc.graphql.dev.vnplatform.com/graphql",
+        {
+          query: `
+            mutation CreateProfile(
+              $firstName: String!,
+              $lastName: String!,
+              $email: String!,
+              $isVerified: Boolean!,
+              $imageUrl: String!,
+              $description: String!
+            ) {
+              createProfile(
+                first_name: $firstName,
+                last_name: $lastName,
+                email: $email,
+                is_verified: $isVerified,
+                image_url: $imageUrl,
+                description: $description
+              ) {
+                id
+                first_name
+                last_name
+                email
+                is_verified
+                image_url
+                description
+              }
+            }
+          `,
+          variables: {
+            firstName,
+            lastName,
+            email,
+            isVerified,
+            imageUrl,
+            description,
+          },
+        },
+        {
+          headers: {
+            Authorization:
+              "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjYW5kaWRhdGVfbmFtZSI6InNhdHlha2lzaG9yZTA0QGdtYWlsLmNvbSIsImlzX2NhbmRpZGF0ZSI6dHJ1ZSwiaWF0IjoxNjg0MjQ2NDQ5LCJleHAiOjE2ODQ3NjQ4NDl9.WsMrvaV1W9f-aVtfBp86nfptBKpfEl3YlSfRcMwiaYw",
+          },
+        }
+      );
+      console.log(response);
+      if (response.data) {
+        document.getElementById("createProfile").reset();
+        window.location.href = "/";
+      }
+    } catch (error) {
+      setError(error.message);
+    }
   };
-  const onFinishFailed = (errorInfo) => {
-    console.log("Failed:", errorInfo);
-  };
+
   return (
     <div>
       <Col span={24}>
@@ -28,29 +158,39 @@ const CreateProfile = () => {
                 </Col>
                 <Col span={2}>
                   <b style={{ fontSize: "20px" }}>
-                    <CloseOutlined
-                      onClick={() => {
-                        window.location.href = "/";
-                      }}
-                    />
+                    {/* <CloseOutlined
+                    onClick={() => {
+                      window.location.href = "/";
+                    }}
+                  /> */}
                   </b>
                 </Col>
               </Row>
             </Col>
-            <hr style={{marginTop:"20px"}}></hr>
+            <hr style={{ marginTop: "20px" }}></hr>
             <Form
-              name="basic"
-              initialValues={{
-                remember: true,
-              }}
+              name="createProfile"
+              // initialValues={{
+              //   remember: true,
+              // }}
               style={{ marginTop: "70px" }}
-              onFinish={onFinish}
-              onFinishFailed={onFinishFailed}
+              onFinish={handleSubmit}
               autoComplete="on"
+              id="createProfile"
+              form={form}
             >
               <span style={{ fontWeight: "bold" }}>Image Link</span>
-              <Form.Item name="username">
+              <Form.Item
+                rules={[
+                  {
+                    required: true,
+                    message: "Please Paste Image URL/Link",
+                  },
+                ]}
+                name="image_url"
+              >
                 <Input
+                  type="link"
                   placeholder="Image Link"
                   style={{ height: "40px", borderRadius: "5px" }}
                 />
@@ -66,7 +206,15 @@ const CreateProfile = () => {
                     xxl={{ span: 12, offset: 0 }}
                   >
                     <span style={{ fontWeight: "bold" }}>First Name</span>
-                    <Form.Item name="password">
+                    <Form.Item
+                      rules={[
+                        {
+                          required: true,
+                          message: "Please Enter First Name",
+                        },
+                      ]}
+                      name="first_name"
+                    >
                       <Input
                         placeholder="Enter your First Name"
                         style={{ height: "40px", borderRadius: "5px" }}
@@ -82,7 +230,15 @@ const CreateProfile = () => {
                     xxl={{ span: 12, offset: 0 }}
                   >
                     <span style={{ fontWeight: "bold" }}>Last Name</span>
-                    <Form.Item name="password">
+                    <Form.Item
+                      rules={[
+                        {
+                          required: true,
+                          message: "Please Enter Last Name",
+                        },
+                      ]}
+                      name="last_name"
+                    >
                       <Input
                         placeholder=" Enter your Last Name"
                         style={{ height: "40px", borderRadius: "5px" }}
@@ -91,15 +247,40 @@ const CreateProfile = () => {
                   </Col>
                 </Row>
               </Col>
+              <span style={{ fontWeight: "bold" }}>Email</span>
+              <Form.Item
+                rules={[
+                  {
+                    required: true,
+                    message: "Please Enter Email Id",
+                  },
+                ]}
+                name="email"
+              >
+                <Input
+                  type="email"
+                  placeholder="Email Id"
+                  style={{ height: "40px", borderRadius: "5px" }}
+                />
+              </Form.Item>
               <span style={{ fontWeight: "bold" }}>Description</span>
-              <Form.Item name="remember" valuePropName="checked">
+              <Form.Item
+                rules={[
+                  {
+                    required: true,
+                    message: "Please Enter Description",
+                  },
+                ]}
+                name="description"
+              >
                 <TextArea
+                  showCount
                   rows={2}
                   placeholder="Write a description for talent"
                 />
               </Form.Item>
               <span style={{ fontWeight: "bold" }}>Verification</span>
-              <Form.Item name="remember" valuePropName="checked">
+              <Form.Item name="is_verified" valuePropName="checked">
                 <div className={styles.talentVerification}>
                   <h6 className={styles.talentIsVerified}>
                     Talent is verified
@@ -108,7 +289,10 @@ const CreateProfile = () => {
                     className={styles.toggle}
                     style={{ width: 43 }}
                     size="default"
-                    defaultChecked
+                    defaultChecked={isVerified}
+                    onChange={() => {
+                      setIsVerified(!isVerified);
+                    }}
                   />
                 </div>
               </Form.Item>
@@ -118,13 +302,7 @@ const CreateProfile = () => {
                   span: 16,
                 }}
               >
-                <Button
-                  type="primary"
-                  htmlType="submit"
-                  onClick={() => {
-                    window.location.href = "/";
-                  }}
-                >
+                <Button onClick={() => {}} type="primary" htmlType="submit">
                   Create Profile
                 </Button>
               </Form.Item>
